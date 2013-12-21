00e5f168150e055c417a644b0562d240643daff3
junonaN <j.vyushkova@gmail.com>
2013-12-21 20:39:08 +0400

    Change configuration in db.yml

--- db/schema.rb
+++ db/schema.rb
@@ -67,8 +67,8 @@ ActiveRecord::Schema.define(version: 20131117123632) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "varieties", force: true do |t|
     t.string   "name"
